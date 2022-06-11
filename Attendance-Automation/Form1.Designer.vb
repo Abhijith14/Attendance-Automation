7b8cf02e40e36b133f9143c567067b7a4f67de3a
Abhijith14 <abhijithukzm@gmail.com>
2022-06-11 15:06:35 +0530

    Updated

---
+++ Attendance-Automation/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(49, 790)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(555, 166)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start Automating"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 14)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 72
+        Me.DataGridView1.RowTemplate.Height = 31
+        Me.DataGridView1.Size = New System.Drawing.Size(1432, 685)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(1491, 148)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(329, 29)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1583, 199)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(146, 55)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(1492, 120)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 25)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(1497, 336)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(322, 32)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(1492, 299)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 25)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1583, 429)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(146, 55)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1855, 1054)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Attendance-Automation/Form1.vb
@@ -0,0 +1,138 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.IO
+Imports System.Text
+Imports ExcelDataReader
+Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Chrome
+Imports OpenQA.Selenium.Support.UI
+Imports WebDriverManager
+Imports WebDriverManager.DriverConfigs.Impl
+
+Public Class Form1
+
+    Dim tables As DataTableCollection
+
+
+    'For Logging in
+    Private Sub Login_fn(driver As IWebDriver, username_txt As String, Password_txt As String)
+
+        driver.Navigate().GoToUrl("https://gcs.edunexttechnologies.com/")
+
+        Dim username As IWebElement = driver.FindElement(By.XPath("//input[@id='username']"))
+        username.SendKeys(username_txt)
+
+        Dim password As IWebElement = driver.FindElement(By.XPath("//input[@id='password']"))
+        password.SendKeys(Password_txt)
+
+        System.Threading.Thread.Sleep(100)
+
+        Dim loginbtn As IWebElement = driver.FindElement(By.XPath("//button[@id='user_login_btn']"))
+        loginbtn.Click()
+
+    End Sub
+
+    ' Open Student Attendance
+
+    Private Sub Open_student_attendance_fn(driver As IWebDriver)
+        Dim stud_att As List(Of IWebElement) = New List(Of IWebElement)
+        Dim cnt As Integer
+        While True
+            stud_att = driver.FindElements(By.XPath("//div[@class='module-box-name']")).ToList()
+            cnt = stud_att.Count
+            If cnt > 0 Then
+                Exit While
+            Else
+                System.Threading.Thread.Sleep(100)
+                Continue While
+            End If
+        End While
+
+        For Each li As IWebElement In stud_att
+            Try
+                If li.Text.Equals("Student Attendance") Then
+                    li.Click()
+                End If
+
+            Catch ex As OpenQA.Selenium.StaleElementReferenceException
+                Console.WriteLine("Got one StaleElement")
+                Continue For
+            End Try
+        Next
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim driver As IWebDriver
+
+        driver = New ChromeDriver
+
+        System.Threading.Thread.Sleep(5000)
+
+        'Login_fn(driver, "GCS1221", "school1398")
+
+        'Open_student_attendance_fn(driver)
+
+        'Dim Sheet As New List(Of String)({"Sheet1", "Sheet2"})
+
+        'For Each it In Sheet
+
+        'Next
+
+        ' get data from table
+
+
+
+        Console.Read()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Using ofd As OpenFileDialog = New OpenFileDialog() With {.Filter = "Excel Workbook|*.xlsx|Excel 97-2003 Workbook|*.xls"}
+            If ofd.ShowDialog() = DialogResult.OK Then
+                TextBox1.Text = ofd.FileName
+                Using stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read)
+                    Using reader As IExcelDataReader = ExcelReaderFactory.CreateReader(stream)
+                        Dim result As DataSet = reader.AsDataSet(New ExcelDataSetConfiguration() With {
+                                                                 .ConfigureDataTable = Function(__) New ExcelDataTableConfiguration() With {
+                                                                 .UseHeaderRow = True}})
+                        tables = result.Tables
+                        ComboBox1.Items.Clear()
+                        For Each table As DataTable In tables
+                            ComboBox1.Items.Add(table.TableName)
+                        Next
+                    End Using
+                End Using
+            End If
+        End Using
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim dt As DataTable = tables(ComboBox1.SelectedItem.ToString())
+        DataGridView1.DataSource = dt
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim dt As DataTable = DataGridView1.DataSource
+
+        'MessageBox.Show(dt.Rows.Count.ToString())
+
+        'Console.WriteLine(dt(0, 0).value)
+        For Each col_value In dt.Columns
+            Console.WriteLine(dt(5))
+        Next
+
+        'For rCnt = 1 To dt.Rows.Count
+        'For cCnt = 1 To dt.Columns.Count
+        'Console.WriteLine(dt(rCnt, cCnt))
+        'Next
+        'Next
+
+        'For Each col_values In dt.Columns
+        ' For Each data_values In dt.Rows
+        'Console.WriteLine(dt(col_values)(data_values))
+        'Next
+        'Next
+
+    End Sub
+End Class
